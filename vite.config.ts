2ae5fed168847495fd735917836ab5b0621dfca7
Jay <jaypalis1999@gmail.com>
2025-06-29 13:25:18 +0800

    config deploy script

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/My-Portfolio/', // ✅ Updated for GitHub Pages
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-});
+});                                                                                                                                               
